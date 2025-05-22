0e5383923fada44ccdb6473754274596c1cef15b
Steven van Dijk <steven.vandijk@pdq.com>
2025-05-22 08:07:28 +0200

    tonie api and yt-dlp

---
+++ lib/tonie/api.ex
@@ -0,0 +1,88 @@
+defmodule Tonie.Api do
+  @api_url "https://api.tonie.cloud/v2/"
+
+  def init do
+    token = auth()
+    household_id = get_household(token)
+    tonies = list_creative_tonies(token, household_id)
+    wizard = hd(tonies)
+
+    %{token: token, household_id: household_id, tonies: tonies, wizard: wizard}
+  end
+
+  @doc """
+  Authenticates with tonie api and returns bearer token
+  """
+  def auth do
+    auth_url = "https://login.tonies.com/auth/realms/tonies/protocol/openid-connect/token"
+
+    data = %{
+      "grant_type" => "password",
+      "client_id" => "my-tonies",
+      "scope" => "openid",
+      "username" => "email@stevenvandijk.com",
+      "password" => "PLACEHOLDER_PASSWORD"
+    }
+
+    %{body: %{"access_token" => token}} = Req.post!(auth_url, form: data)
+    token
+  end
+
+  @doc """
+  Assumes 1 household and returns the household id
+  """
+  def get_household(token) do
+    url = "#{@api_url}households"
+
+    %{body: [%{"id" => id}]} = Req.get!(url, auth: {:bearer, token})
+    id
+  end
+
+  @doc """
+  Returns the list of creative tonies for the given household
+  """
+  def list_creative_tonies(token, household_id) do
+    url = "#{@api_url}households/#{household_id}/creativetonies"
+
+    %{body: tonies} = Req.get!(url, auth: {:bearer, token})
+    Enum.map(tonies, &%{"id" => &1["id"], "imageUrl" => &1["imageUrl"]})
+  end
+
+  def clear_creative_tonie(token, household_id, tonie) do
+    url = "#{@api_url}households/#{household_id}/creativetonies/#{tonie["id"]}"
+
+    Req.patch!(url, json: %{chapters: []}, auth: {:bearer, token})
+    :ok
+  end
+
+  def upload_file(token, household_id, tonie, file_path) do
+    url = "https://api.tonie.cloud/v2/file"
+    # Guess MIME type based on file extension
+    mime_type =
+      case Path.extname(file_path) do
+        ".mp3" -> "audio/mpeg"
+        ".wav" -> "audio/wav"
+        ".aac" -> "audio/aac"
+        ".ogg" -> "audio/ogg"
+        _ -> "application/octet-stream"
+      end
+
+    %{body: %{"fileId" => file_id, "request" => %{"fields" => fields, "url" => upload_url}}} =
+      Req.post!(url, auth: {:bearer, token})
+
+    file_content = File.read!(file_path)
+
+    form_data =
+      Map.to_list(fields) ++
+        [file: {file_content, filename: file_id, content_type: mime_type}]
+
+    %{status: 204} = Req.post!(upload_url, form_multipart: form_data)
+
+    # assign file to tonie
+    title = Path.basename(file_path, Path.extname(file_path))
+
+    chapters_url = "#{@api_url}households/#{household_id}/creativetonies/#{tonie["id"]}/chapters"
+
+    Req.post!(chapters_url, json: %{title: title, file: file_id}, auth: {:bearer, token})
+  end
+end
---
+++ lib/tonie/yt_dlp/yt_dlp.ex
@@ -0,0 +1,38 @@
+defmodule Tonie.YtDlp do
+  def download(url) do
+    path = "./lib/tonie/yt_dlp/binaries/mac/"
+    yt_dlp_path = Path.join(path, "yt-dlp") |> Path.expand() |> executable_path
+    ffmpeg_path = Path.join(path, "ffmpeg") |> Path.expand() |> executable_path
+
+    options =
+      arguments([
+        "extract-audio",
+        {"audio-format", "mp3"},
+        {"audio-quality", "0"},
+        {"paths", "home:./downloads"},
+        {"output", "%(playlist_index)s-%(title)s.%(ext)s"},
+        {"ffmpeg-location", ffmpeg_path}
+      ]) ++ [url]
+
+    case System.cmd(yt_dlp_path, options, stderr_to_stdout: true) do
+      {"", error} -> {:error, error}
+      {error, 2} -> {:error, error}
+      {meta, 0} -> {:ok, meta}
+      {meta, 1} -> {:error, meta}
+    end
+  end
+
+  defp arguments(options) do
+    options
+    |> Enum.reduce([], fn
+      {k, v}, acc -> acc ++ ["--#{k}", v]
+      argument, acc -> ["--#{argument}" | acc]
+    end)
+  end
+
+  defp executable_path(path) do
+    File.chmod!(path, 0o755)
+
+    path
+  end
+end
