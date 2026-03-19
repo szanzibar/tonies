7aac844d46751c830b1403d83867582dc7154977
Steven van Dijk <steven.vandijk@pdq.com>
2026-03-19 22:46:42 +0100

    favicon

--- lib/tonie_web/live/youtube_uploader_live.ex
+++ lib/tonie_web/live/youtube_uploader_live.ex
@@ -280,8 +280,8 @@ defmodule TonieWeb.YoutubeUploaderLive do
 
         <button
           type="submit"
-          class="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
-          disabled={@status != :idle}
+          class="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+          disabled={@status != :idle || @selected_tonie_id == nil}
         >
           Start Upload
         </button>
