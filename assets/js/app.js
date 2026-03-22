9bfbb91e12b56300ba0bf31ec1d6a28b584e459a
Steven van Dijk <steven.vandijk@pdq.com>
2026-03-22 13:11:24 +0100

    pwa

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,3 +57,9 @@ liveSocket.connect();
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket;
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
