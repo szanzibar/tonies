650fc717ca745b74ee53654ba2e88acb0a9a513a
Steven van Dijk <steven.vandijk@pdq.com>
2026-04-01 22:18:09 +0200

    fix port variable

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,6 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :tonie, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
@@ -60,7 +59,7 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: 4000
     ],
     secret_key_base: secret_key_base
 
